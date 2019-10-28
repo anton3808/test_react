7a6ef7bd7d1e32eb908f6fc7387bb7515e2adecd
Anton <antonsadlov@gmail.com>
2019-10-28 23:59:45 +0200

    18 tutorial Dialog page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 
 
@@ -12,7 +13,11 @@ const App = () => {
       <Header />
       <Navbar />
 
-      <Profile />
+      {/* <Profile /> */}
+
+      <div className="app-wrapper-content">
+        <Dialogs />
+      </div>
 
     </div>
   );
