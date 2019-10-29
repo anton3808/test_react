284480fe7e91a13fe577641af95be5f9bb767a2d
Anton <antonsadlov@gmail.com>
2019-10-30 01:39:19 +0200

    19 tutorial route, browser-router

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
+    <BrowserRouter>{/* обгортка для route */}
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
 
-      {/* <Profile /> */}
+        <div className="app-wrapper-content">
+          {/* Route використовується для роботи як силки, path - адрес сторінки, component - в цей параметр ми передаєм компоненту яка буде віддображатись */}
+          <Route path="/dialogs" component={Dialogs} /> 
+          <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/Settings" component={Settings} />
+        </div>
 
-      <div className="app-wrapper-content">
-        <Dialogs />
       </div>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
