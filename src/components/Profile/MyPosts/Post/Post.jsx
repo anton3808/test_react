576cab2acd723d9aa613a22d72b81be76a9f9757
Anton <antonsadlov@gmail.com>
2019-10-30 18:13:43 +0200

    24 tutorial (UI - BLL)

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -10,7 +10,7 @@ const Post = (props) => {
       <img src="https://hornews.com/images/news_large/c1d4b2b8ec608ea72764c5678816d5c9.jpg" alt="" />
       {props.message}
       <div>
-        <span>like</span>
+        <span>like {props.likesCount}</span>
       </div>
     </div>
 
