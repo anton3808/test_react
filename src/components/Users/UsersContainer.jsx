8f69b89c4ece3c19543d546f4e8cbec64a3c7746
Anton <antonsadlov@gmail.com>
2020-01-03 20:43:34 +0200

    56 tutorial (Презентационая и Контейнерная компоненты)

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 import {connect} from "react-redux";
-import Users from './Users';
 import { setUsersTotalCountAC, setCurrentPageAC, followAC, unfollowAC, setUsersAC } from '../../redux/users-reducer';
+import * as axios from 'axios'; //библиотека для запросові
+import Users from './Users';
+
+
+class UsersContainer extends React.Component{
+
+  constructor (props) {//вывивается только один раз коград создается обэкт класса
+    super(props);
+  }
+
+  componentDidMount(){ //вмонтирование (только один раз)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+      this.props.setUsers(response.data.items);
+      this.props.setTotalUsersCount(response.data.totalCount);
+    });
+  }
+
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${ pageNumber }&count=${this.props.pageSize}`).then(response => {
+      this.props.setUsers(response.data.items);
+    });
+  }
+
+  render() {
+    return <Users totalUsersCount = { this.props.totalUsersCount }
+                  pageSize = { this.props.pageSize }
+                  currentPage = { this.props.currentPage }
+                  onPageChanged = { this.onPageChanged }
+                  users = { this.props.users }
+                  unfollow = { this.props.unfollow }
+                  follow = { this.props.follow }
+                                    
+    /> 
+  }
+  
+}
 
 let mapStateToProps = (state) => {//с помощью этой функции прийдет в функциональную компоненту Users в Props будет сидеть свойство users значением которого будут пользователи из state
   return {
@@ -39,4 +76,4 @@ let mapDispatchToProps = (dispatch) => {//передаем callbacks functions
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users) ;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer) ;
